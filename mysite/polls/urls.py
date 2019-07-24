b5a9885043bc7dbd1ba0f623dd31a062d71018a7
sjKim <danier12345@gmail.com>
2019-07-24 15:56:09 +0900

    - urls.py 수정

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),                # django 예제
     path('stock/', views.stockwatch, name='stockwatch'),                    # 초기 화면
     path('stock_result/', views.stock_result, name='stock_result'),     # 검색 결과 rendering 화면
-    # path('side', views.search_by_ic, name='side'),
     # path('stock_add', views.db_add, name='db_update'),
     # stock_add 는 초기에 DB에 모든 종목 넣는 용도로 사용.(한번만 사용하고 주석으로 잠그자)
     # git test
