f356dd028abf57c1e7f119884c4f959a7a37e286
sjKim <danier12345@gmail.com>
2019-07-24 11:12:34 +0900

    Commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('side', views.search_by_ic, name='side'),
     #path('stock_add', views.db_add, name='db_update'),
     # stock_add 는 초기에 DB에 모든 종목 넣는 용도로 사용.(한번만 사용하고 주석으로 잠그자)
+    # git test
 
 ]
